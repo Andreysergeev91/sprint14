7d834623cc2af2118bb71a6086ca76de46f7fc40
Andrey Sergeev <andreyser07@gmail.com>
2020-06-02 00:15:54 +0700

    Добавлены email и password, к схеме user
    Добавлены два поля email и password, а также метод findUserByCredentials

--- models/user.js
+++ models/user.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcryptjs');
+
 const validate = require('mongoose-validator');
 
 const avatarLinkValidator = [
   validate({
     validator: 'isURL',
     message: 'There should be a link in this field'
+  })
+];
+
+const emailValidator = [
+  validate({
+    validator: 'isEmail',
+    message: 'There should be an Email adress in this field'
   })
 ];
 
@@ -26,7 +35,37 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate: avatarLinkValidator
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: emailValidator
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+    minlength: 8
   }
 });
+
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
