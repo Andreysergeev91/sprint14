dea26a5411226b763d3eafdec8d122b1a1e573de
Andrey Sergeev <andreyser07@gmail.com>
2020-06-07 02:03:42 +0700

    Модули helmet и express-rate-limit добавлены.
    Модули helmet и express-rate-limit добавлены. Добавлена обработка ошибок
    для mongoose.connect.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const rateLimit = require('express-rate-limit');
+
+const helmet = require('helmet');
+
 const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
@@ -11,11 +15,21 @@ const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 
 const {
-  PORT = 3000
+  PORT = 3000,
 } = process.env;
 
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+
+app.use(limiter);
+
+app.use(helmet());
 
 app.use(cookieParser());
 
@@ -26,8 +40,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useFindAndModify: false
-});
+  useFindAndModify: false,
+// eslint-disable-next-line no-console
+}).catch((err) => console.error(err));
 
 app.post('/signup', createUser);
 app.post('/signin', login);
@@ -40,7 +55,7 @@ app.use('/', require('./routes/cards'));
 
 app.all('*', (req, res) => {
   res.status(404).send({
-    message: 'Запрашиваемый ресурс не найден'
+    message: 'Запрашиваемый ресурс не найден',
   });
 });
 
